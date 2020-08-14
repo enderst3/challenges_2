88908352900bc942f828a7d3a497abf3e45b7177
enderst3 <enderst3@gmail.com>
2020-08-14 14:30:11 -0700

    created function

--- python/number_generator.py
+++ python/number_generator.py
@@ -4,10 +4,13 @@ random number generator with unser input
 
 import random
 
+def number_generator():
+    print("Welcome to the random number generator!")
 
-print("Welcome to the random number generator!")
+    start= int(input("What number do you want to start with? "))
+    end = int(input("What number do you want to end with? "))
 
-start= int(input("What number do you want to start with? "))
-end = int(input("What number do you want to end with? "))
+    print("Your random number is {}".format(random.randint(start, end)))
 
-print("Your random number is {}".format(random.randint(start, end)))
+
+number_generator()
