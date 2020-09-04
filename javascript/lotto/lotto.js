1adcb15c6d27b4884440aa951b6d85120563d0d2
enderst3 <enderst3@gmail.com>
2020-09-04 15:01:09 -0700

    powerball, and picks add

--- javascript/lotto/lotto.js
+++ javascript/lotto/lotto.js
@@ -14,14 +14,26 @@ let lottoNumbers = []
 let powerNumber = []
 let picks = Math.floor(document.getElementById('picks').value)
 let max = Math.floor(document.getElementById("max").value)
-let addedPick = Math.floor(document.getElementById('addedPick').value)
 let addedMax = Math.floor(document.getElementById('addedMax').value)
 
 
 // number gererator takes the max from input
 function numberGen() {
-    let max = Math.floor(document.getElementById("max").value)
-    // the +1 makes sure the max is included
-    let randNum = Math.floor((Math.random() * max) + 1)
-    return randNum
+    // the +1 makes sure the max number is included
+    //
+    console.log(picks, max, addedMax)
+    while(lottoNumbers.length < picks) {
+        let randNum = Math.floor((Math.random() * max) + 1)
+        if(! lottoNumbers.includes(randNum)){
+            lottoNumbers.push(randNum)
+        }  
+    }
+
+    if(addedMax) {
+        powerNumber.push(Math.floor((Math.random() * addedMax) + 1))
+        console.log(powerNumber)
+    }
+    console.log(lottoNumbers)
+    
+    
 }
