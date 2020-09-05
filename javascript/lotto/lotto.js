0bf46b9201573bcc12ecee47d224168e32aed3c4
enderst3 <enderst3@gmail.com>
2020-09-05 14:31:47 -0700

    notes and sort list

--- javascript/lotto/lotto.js
+++ javascript/lotto/lotto.js
@@ -9,31 +9,36 @@
 // need to logic for amount of numbers to choose
 // add logic if numbers have different ranges like powerball
 
-
+// create list for random numbers based on number of picks and range
 let lottoNumbers = []
+// if there is an extra pick with different number range it will add to this list
 let powerNumber = []
+// get input on number of picks
 let picks = Math.floor(document.getElementById('picks').value)
+// get the number range for the picks
 let max = Math.floor(document.getElementById("max").value)
+// get imput on range of extra pick with different range
 let addedMax = Math.floor(document.getElementById('addedMax').value)
 
 
 // number gererator takes the max from input
 function numberGen() {
+    // pick random numbers and check to see if in list before adding them
     // the +1 makes sure the max number is included
-    //
-    console.log(picks, max, addedMax)
     while(lottoNumbers.length < picks) {
+        // pick the numbers
         let randNum = Math.floor((Math.random() * max) + 1)
+        // check to see if already picked before adding to list
         if(! lottoNumbers.includes(randNum)){
             lottoNumbers.push(randNum)
         }  
     }
-
+    // pick extra ball if needed
     if(addedMax) {
         powerNumber.push(Math.floor((Math.random() * addedMax) + 1))
-        console.log(powerNumber)
     }
-    console.log(lottoNumbers)
-    
-    
+    // sort numbers before displaying them
+    // lottoNumbers.sort(function(a, b){return a - b})
+    console.log(lottoNumbers.sort(function(a, b){return a - b}))
+    console.log(powerNumber)  
 }
