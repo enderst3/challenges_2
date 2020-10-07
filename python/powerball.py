90f53609000f4c38e6573c8ce7c44828000baf33
enderst3 <enderst3@gmail.com>
2020-10-06 21:42:44 -0700

    powerball working

--- python/powerball.py
+++ python/powerball.py
@@ -9,11 +9,10 @@ import random
 
 # ticket balls
 ticket_balls = 0
-ticket_power_ball = 0
+
 
 # lotto balls
 lotto_balls = 1
-power_ball = 1
 
 # number of plays
 plays = 0
@@ -21,17 +20,22 @@ plays = 0
 # while loop to pick balls and compare tickets and lotto numbers
 while ticket_balls != lotto_balls:
     # pick ticket using random.sample()
-    ticket_balls= sorted(random.sample(range(1, 70), 5))
+    ticket_balls = sorted(random.sample(range(1, 70), 5))
     ticket_power_ball = random.randint(1, 26)
+    ticket_balls.append(ticket_power_ball)
     
     # pick lotto_balls using random.sample()
     lotto_balls = sorted(random.sample(range(1, 70), 5))
     power_ball = random.randint(1, 26)
-
-    if ticket_balls != lotto_balls or ticket_power_ball != power_ball:
-        plays += 1
+    lotto_balls.append(power_ball)
     
+
+    if ticket_balls != lotto_balls:
+        plays += 1
+
     else:
         print("It took you this many tries to win {}".format(plays))
-        print("The winning numbers are: {} {}".format(lotto_balls, power_ball))
-        print("The winning ticket had these numbers: {} {}".format(ticket_balls, ticket_power_ball))
+        print("The winning numbers are: {}".format(lotto_balls))
+        print("The winning ticket had these numbers: {}".format(ticket_balls))
+        
+    
