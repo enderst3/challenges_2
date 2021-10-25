8a55b6f9c8a176558eb917e73ae7cddc38315945
enderst3 <enderst3@gmail.com>
2021-10-24 21:29:59 -0700

    maximize

---
+++ python/maximize.py
@@ -0,0 +1,18 @@
+"""
+Sample Input
+3 1000
+2 5 4
+3 7 8 9 
+5 5 7 8 9 10 
+
+Sample Output
+206
+
+"""
+
+from itertools import product
+
+# k, m = map(int, input().split())
+# l = [list(map(int, input().split()))[1:] for _ in range(k)]
+
+# print(max(map(lambda x: sum(1**2 for i in x)%m, product(*l))))
