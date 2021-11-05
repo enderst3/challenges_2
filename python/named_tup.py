78440db8c4073b1d190a345f930e9a003adf343d
enderst3 <enderst3@gmail.com>
2021-11-05 13:13:04 -0700

    namedtuple

--- python/named_tup.py
+++ python/named_tup.py
@@ -28,3 +28,14 @@ TESTCASE 02
 81.00
 
 '''
+
+from collections import namedtuple
+
+n = int(input())
+a = input()
+total = 0
+Student = namedtuple('Student', a)
+for i in range(n):
+    student = Student(*input().split())
+    total += int(student.MARKS)
+print('{:.2f}'.format(total/n))
