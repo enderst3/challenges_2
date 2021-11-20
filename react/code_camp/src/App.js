00d209b6a4c07de0ff0ed61a31ce222b732be1e1
enderst3 <enderst3@gmail.com>
2021-11-19 19:55:34 -0800

    react

---
+++ react/code_camp/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <h1>Hello World</h1>,
+  document.getElementById('root')
+);
+
+export default App;
