e1520f6bd3f6955b51208fe6d3d25b968b96cf7d
enderst3 <enderst3@gmail.com>
2021-11-22 21:54:53 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -14,10 +14,13 @@ function App() {
   );
 }
 
+
 const JSX = (
-  <div>
+  <div className='myDiv'>
+    {/* Test jsx notes */}
     <h1>This is a block of JSX</h1>
     <p>Here's a subtitle</p>
+
   </div>
 )
 
