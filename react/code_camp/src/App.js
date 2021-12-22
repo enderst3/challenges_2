2b73e5254b89ae5f0a8a1984c3ee0ce51c8c2e04
enderst3 <enderst3@gmail.com>
2021-12-21 20:41:54 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -15,41 +15,82 @@ function App() {
 }
 
 
-const ChildComponent = () => {
-  return (
-    <div>
-      <p>I am the child</p>
-    </div>
-  )
-}
+// const ChildComponent = () => {
+//   return (
+//     <div>
+//       <p>I am the child</p>
+//     </div>
+//   )
+// }
 
 
-class ParentComponent extends React.Component {
-  constructor(props) { 
-    super(props)
-  }
-  render() {
-    return (
-      <div>
-        <h1>I am the parent</h1>
-        <ChildComponent/>
-      </div>
-    )
+// class ParentComponent extends React.Component {
+//   constructor(props) { 
+//     super(props)
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>I am the parent</h1>
+//         <ChildComponent/>
+//       </div>
+//     )
 
 
     
-  }
-}
+//   }
+// }
+
+// const TypesOfFruit = () => {
+//   return (
+//     <div>
+//       <h2>Fruits:</h2>
+//       <ul>
+//         <li>Apples</li>
+//         <li>Blueberries</li>
+//         <li>Strawberries</li>
+//         <li>Bananas</li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const Fruits = () => {
+//   return (
+//     <div>
+//       { /* Change code below this line */ }
+//       <TypesOfFruit/>
+//       { /* Change code above this line */ }
+//     </div>
+//   );
+// };
+
+// class TypesOfFood extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Types of Food:</h1>
+//         { /* Change code below this line */ }
+//           <Fruits/>
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
 
 
-// const JSX = (
-//   <div className='myDiv'>
-//     {/* Test jsx notes */}
-//     <h1>This is a block of JSX</h1>
-//     <p>Here's a subtitle</p>
+const JSX = (
+  <div className='myDiv'>
+    {/* Test jsx notes */}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
 
-//   </div>
-//)
+  </div>
+)
 
 ReactDOM.render(
   JSX,
