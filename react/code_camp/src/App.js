c0851380114d990468f59da70316270609b88b3d
enderst3 <enderst3@gmail.com>
2022-01-09 21:28:25 -0800

    react state

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -214,6 +214,24 @@ function App() {
 //   name: PropTypes.string.isRequired
 // }
 
+class StatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    // Only change code below this line
+    this.state = {
+      name: 'name'
+    }
+    // Only change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
 
 const JSX = (
   <div className='myDiv'>
