e61c0b860dd723ff8cffddfc105e14d9db857879
enderst3 <enderst3@gmail.com>
2022-01-11 20:34:29 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -255,30 +255,30 @@ function App() {
 //   }
 // };
 
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Initial State'
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    // Change code below this line
-    this.setState({
-      name: 'React Rocks!'
-    })
-    // Change code above this line
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Click Me</button>
-        <h1>{this.state.name}</h1>
-      </div>
-    );
-  }
-};
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'Initial State'
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//   handleClick() {
+//     // Change code below this line
+//     this.setState({
+//       name: 'React Rocks!'
+//     })
+//     // Change code above this line
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>Click Me</button>
+//         <h1>{this.state.name}</h1>
+//       </div>
+//     );
+//   }
+// };
 
 const JSX = (
   <div className='myDiv'>
