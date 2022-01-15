f9e148df35a5d1393c514f547db30b84f93767a3
enderst3 <enderst3@gmail.com>
2022-01-14 21:19:47 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -280,28 +280,105 @@ function App() {
 //   }
 // };
 
-class MyComponent extends React.Component {
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       text: "Hello"
+//     };
+//     // Change code below this line
+//     this.handleClick = this.handleClick.bind(this)
+//     // Change code above this line
+//   }
+//   handleClick() {
+//     this.setState({
+//       text: "You clicked!"
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         { /* Change code below this line */ }
+//         <button onClick={this.handleClick}>Click Me</button>
+//         { /* Change code above this line */ }
+//         <h1>{this.state.text}</h1>
+//       </div>
+//     );
+//   }
+// };
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       visibility: false
+//     };
+//     // Change code below this line
+//     this.toggleVisibility = this.toggleVisibility.bind(this)
+//     // Change code above this line
+//   }
+//   // Change code below this line
+//   toggleVisibility() {
+//     this.setState(state => ({
+//       visibility: !state.visibility
+//     }))
+//   }
+//   // Change code above this line
+//   render() {
+//     if (this.state.visibility) {
+//       return (
+//         <div>
+//           <button onClick={this.toggleVisibility}>Click Me</button>
+//           <h1>Now you see me!</h1>
+//         </div>
+//       );
+//     } else {
+//       return (
+//         <div>
+//           <button onClick={this.toggleVisibility}>Click Me</button>
+//         </div>
+//       );
+//     }
+//   }
+// }
+
+class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: "Hello"
+      count: 0
     };
     // Change code below this line
-    this.handleClick = this.handleClick.bind(this)
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.reset = this.reset.bind(this)
     // Change code above this line
   }
-  handleClick() {
-    this.setState({
-      text: "You clicked!"
-    });
+  // Change code below this line
+  increment() {
+    this.setState(state => ({
+      count: state.count + 1
+    }))
   }
+  decrement() {
+    this.setState(state => ({
+      count: state.count - 1
+    }))
+  }
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+
+  // Change code above this line
   render() {
     return (
       <div>
-        { /* Change code below this line */ }
-        <button onClick={this.handleClick}>Click Me</button>
-        { /* Change code above this line */ }
-        <h1>{this.state.text}</h1>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
       </div>
     );
   }
