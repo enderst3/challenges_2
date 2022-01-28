be2c2c146c3679ddd70d707f49dfcfb03f6ea077
enderst3 <enderst3@gmail.com>
2022-01-27 19:39:30 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -461,38 +461,115 @@ function App() {
 //   }
 // }
 
+// class MyApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'CamperBot'
+//     }
+//   }
+//   render() {
+//     return (
+//        <div>
+//          {/* Change code below this line */}
+//          <Navbar 
+//           name = {this.state.name}
+//          />
+//          {/* Change code above this line */}
+//        </div>
+//     );
+//   }
+// };
+
+// class Navbar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//     <div>
+//       {/* Change code below this line */}
+//       <h1>Hello, my name is: {this.props.name}</h1>
+//       {/* Change code above this line */}
+//     </div>
+//     );
+//   }
+// };
+
 class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'CamperBot'
+      inputValue: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
   }
   render() {
     return (
        <div>
-         {/* Change code below this line */}
-         <Navbar />
-         {/* Change code above this line */}
+        { /* Change code below this line */ }
+        <GetInput
+          input={this.state.inputValue}
+          handleChange={this.handleChange}
+        />
+        <RenderInput
+          input={this.state.inputValue}
+        />
+        { /* Change code above this line */ }
        </div>
     );
   }
 };
 
-class Navbar extends React.Component {
+class GetInput extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-    <div>
-      {/* Change code below this line */}
-      <h1>Hello, my name is: </h1>
-      {/* Change code above this line */}
-    </div>
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
     );
   }
 };
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log('component mounted')
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
+
 
 
 const JSX = (
