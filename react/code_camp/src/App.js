3bd9ca907cb47b1d8971b4efb9f8386d4ff7ef4a
enderst3 <enderst3@gmail.com>
2022-02-02 16:18:53 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -570,26 +570,106 @@ function App() {
 //   }
 // };
 
-class MyComponent extends React.Component {
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activeUsers: null
+//     };
+//   }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({
+//         activeUsers: 1273
+//       });
+//     }, 2500);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {/* Change code below this line */}
+//         <h1>Active Users: {this.state.activeUsers}</h1>
+//         {/* Change code above this line */}
+//       </div>
+//     );
+//   }
+// }
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       message: ''
+//     };
+//     this.handleEnter = this.handleEnter.bind(this);
+//     this.handleKeyPress = this.handleKeyPress.bind(this);
+//   }
+//   // Change code below this line
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.handleKeyPress)
+//   }
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.handleKeyPress)
+//   }
+//   // Change code above this line
+//   handleEnter() {
+//     this.setState((state) => ({
+//       message: state.message + 'You pressed the enter key! '
+//     }));
+//   }
+//   handleKeyPress(event) {
+//     if (event.keyCode === 13) {
+//       this.handleEnter();
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.message}</h1>
+//       </div>
+//     );
+//   }
+// };
+
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Change code below this line
+    if (nextProps.value % 2 == 0){    
+      return true
+    }
+    return false
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeUsers: null
+      value: 0
     };
+    this.addValue = this.addValue.bind(this);
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        activeUsers: 1273
-      });
-    }, 2500);
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
   }
   render() {
     return (
       <div>
-        {/* Change code below this line */}
-        <h1>Active Users: {this.state.activeUsers}</h1>
-        {/* Change code above this line */}
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
       </div>
     );
   }
