3885e235002bca8d7dd84a9b1280314993d99044
enderst3 <enderst3@gmail.com>
2022-02-05 17:08:51 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -775,6 +775,38 @@ function App() {
 //   }
 // }
 
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       display: true
+//     }
+//     this.toggleDisplay = this.toggleDisplay.bind(this);
+//   }
+//   toggleDisplay() {
+//     this.setState((state) => ({
+//       display: !state.display
+//     }));
+//   }
+//   render() {
+//     // Change code below this line
+//     if (this.state.display) {
+//       return (
+//          <div>
+//            <button onClick={this.toggleDisplay}>Toggle Display</button>
+//            <h1>Displayed!</h1>
+//          </div>
+//       );
+//     } else {
+//       return (
+//         <div>
+//            <button onClick={this.toggleDisplay}>Toggle Display</button>
+//          </div>
+//       );
+//     }
+//   }
+// };
+
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -784,28 +816,21 @@ class MyComponent extends React.Component {
     this.toggleDisplay = this.toggleDisplay.bind(this);
   }
   toggleDisplay() {
-    this.setState((state) => ({
+    this.setState(state => ({
       display: !state.display
     }));
   }
   render() {
     // Change code below this line
-    if (this.state.display) {
-      return (
-         <div>
-           <button onClick={this.toggleDisplay}>Toggle Display</button>
-           <h1>Displayed!</h1>
-         </div>
-      );
-    } else {
-      return (
-        <div>
-           <button onClick={this.toggleDisplay}>Toggle Display</button>
-         </div>
-      );
-    }
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         {this.state.display && <h1>Displayed!</h1>}
+       </div>
+    );
   }
 };
+
 
 const JSX = (
   <div className='myDiv'>
