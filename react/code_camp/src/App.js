f7adc98b6a68b576311109ef456a52b98592eb4f
enderst3 <enderst3@gmail.com>
2022-02-11 17:53:45 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -926,38 +926,82 @@ function App() {
 //   }
 // }
 
-class GateKeeper extends React.Component {
+// class GateKeeper extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       input: ''
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ input: event.target.value })
+//   }
+//   render() {
+//     let inputStyle = {
+//       border: '1px solid black'
+//     };
+//     // Change code below this line
+//     if (this.state.input.length > 15) {
+//       inputStyle.border = '3px solid red'
+//     } 
+//     // Change code above this line
+//     return (
+//       <div>
+//         <h3>Don't Type Too Much:</h3>
+//         <input
+//           type="text"
+//           style={inputStyle}
+//           value={this.state.input}
+//           onChange={this.handleChange} />
+//       </div>
+//     );
+//   }
+// };
+
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: ''
-    };
+    // Change code below this line
+
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-  handleChange(event) {
-    this.setState({ input: event.target.value })
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
   }
   render() {
-    let inputStyle = {
-      border: '1px solid black'
-    };
-    // Change code below this line
-    if (this.state.input.length > 15) {
-      inputStyle.border = '3px solid red'
-    } 
-    // Change code above this line
+    const items = null; // Change this line
     return (
       <div>
-        <h3>Don't Type Too Much:</h3>
-        <input
-          type="text"
-          style={inputStyle}
-          value={this.state.input}
-          onChange={this.handleChange} />
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
       </div>
     );
   }
-};
+}
 
 const JSX = (
   <div className='myDiv'>
