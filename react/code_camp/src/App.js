289583068e8bb8c33cd4136af6de8a14b22b34eb
enderst3 <enderst3@gmail.com>
2022-02-17 16:17:48 -0800

    react

--- react/code_camp/src/App.js
+++ react/code_camp/src/App.js
@@ -959,52 +959,73 @@ function App() {
 //   }
 // };
 
-const textAreaStyles = {
-  width: 235,
-  margin: 5
-};
+// const textAreaStyles = {
+//   width: 235,
+//   margin: 5
+// };
 
-class MyToDoList extends React.Component {
-  constructor(props) {
-    super(props);
-    // Change code below this line
-    this.state ={
-      userInput: '',
-      toDoList: []
-    }
-    // Change code above this line
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleSubmit() {
-    const itemsArray = this.state.userInput.split(',');
-    this.setState({
-      toDoList: itemsArray
-    });
-  }
-  handleChange(e) {
-    this.setState({
-      userInput: e.target.value
-    });
-  }
-  render() {
-    const items = this.state.toDoList.map(i => <li>{i}</li>); // Change this line
-    return (
-      <div>
-        <textarea
-          onChange={this.handleChange}
-          value={this.state.userInput}
-          style={textAreaStyles}
-          placeholder='Separate Items With Commas'
-        />
-        <br />
-        <button onClick={this.handleSubmit}>Create List</button>
-        <h1>My "To Do" List:</h1>
-        <ul>{items}</ul>
-      </div>
-    );
-  }
-}
+// class MyToDoList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // Change code below this line
+//     this.state ={
+//       userInput: '',
+//       toDoList: []
+//     }
+//     // Change code above this line
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleSubmit() {
+//     const itemsArray = this.state.userInput.split(',');
+//     this.setState({
+//       toDoList: itemsArray
+//     });
+//   }
+//   handleChange(e) {
+//     this.setState({
+//       userInput: e.target.value
+//     });
+//   }
+//   render() {
+//     const items = this.state.toDoList.map(i => <li>{i}</li>); // Change this line
+//     return (
+//       <div>
+//         <textarea
+//           onChange={this.handleChange}
+//           value={this.state.userInput}
+//           style={textAreaStyles}
+//           placeholder='Separate Items With Commas'
+//         />
+//         <br />
+//         <button onClick={this.handleSubmit}>Create List</button>
+//         <h1>My "To Do" List:</h1>
+//         <ul>{items}</ul>
+//       </div>
+//     );
+//   }
+// }
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = null; // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
 
 const JSX = (
   <div className='myDiv'>
