8ee2dcc725082fc885fac6917a87af06379425ff
enderst3 <enderst3@gmail.com>
2022-03-04 20:22:25 -0800

    project1

--- react/projects_course/project1/src/index.js
+++ react/projects_course/project1/src/index.js
@@ -1,10 +1,18 @@
-import ReactDom from 'react-dom'
+import React from 'react';
+import { createElement } from 'react';
+import ReactDom from 'react-dom';
 
-function Greeting(){
-  return <h4>This is my first component!</h4>
+
+// normal function
+function Greeting() {
+    return (
+        <h4>This is my first react component!</h4>
+    )
 }
 
-
-
+// arrow function
+// const Greeting = () => {
+//     return  React.createElement('h4', {}, "This is my first react arrow component")
+// }
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
