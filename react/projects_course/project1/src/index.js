ce7ff77c4f3dd3eee0dae2b23a75d3470c04700b
enderst3 <enderst3@gmail.com>
2022-03-31 19:32:40 -0700

    project1

--- react/projects_course/project1/src/index.js
+++ react/projects_course/project1/src/index.js
@@ -6,30 +6,47 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css'
 
-// Setup vars
-const author = 'Amelia Hepworth'
-const title = 'I Love You to the Moon and Back'
-const img = "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX258_BO1,204,203,200_.jpg"
-
+// create data list 
+const books = [
+    {
+        id: 1,
+        author: 'Amelia Hepworth',
+        title: 'I Love You to the Moon and Back',
+        img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX258_BO1,204,203,200_.jpg",
+    },
+    {
+        id: 2,
+        author: 'Margaret Wise Brown, Clement Hurd',
+        title: 'Goodnight Moon',
+        img: "https://images-na.ssl-images-amazon.com/images/I/51kpoART0HL._AC_SX368_.jpg"
+    },
+    {
+        id: 3,
+        author: 'Eric Carle',
+        title: 'The Very Hungry Caterpillar',
+        img: "https://images-na.ssl-images-amazon.com/images/I/41w4B0f21VL._AC_SX368_.jpg"
+    }
+]
 function BookList() {
     return (
         <section className='bookList'>
-            <Book/>
+            {books.map((book) => {
+                const { img, title, author } = book;
+                return <Book key={book.id} {...book}></Book>
+            })}
         </section>
     )   
 }
 
 
 const Book = (props) => {
-        return (
-        <article className='book'>
-            <img 
-                src = {img}
-                alt = ''
-            />
-            <h1>{title}</h1>
-            <h3>{author}</h3>
-        </article>
+    const { img, title, author } = props;
+    return (
+    <article className='book'>
+        <img src={img} alt="" />
+        <h1>{title}</h1>
+        <h3>{author}</h3>
+    </article>
     )
 }
 
