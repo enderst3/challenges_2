6063c4ae6d7e29f8e2a9e2956be024ca7d527047
enderst3 <enderst3@gmail.com>
2022-04-08 19:59:18 -0700

    project_2

--- react/react-project-2/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react/react-project-2/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+
+  // its convention to ues set then the variable
+  const [text, setText] = useState('random title')
+
+  // handle click then use logic to toggle back and forth between text on clicks
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText("Hello World")
+    } else {
+      setText('random title')
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button
+        className='btn'
+        onClick={handleClick}
+      >
+        Change the Title!
+      </button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
