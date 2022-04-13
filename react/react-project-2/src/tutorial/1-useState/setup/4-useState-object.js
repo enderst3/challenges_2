be7bfd5bc9bf4f39bae2a758f0abd9d2fe0f27b7
enderst3 <enderst3@gmail.com>
2022-04-12 19:38:30 -0700

    project_2

--- react/react-project-2/src/tutorial/1-useState/setup/4-useState-object.js
+++ react/react-project-2/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  const [person, setPerson] = useState()
-  return <h2>For this one we will use an object!</h2>;
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 24,
+    message: 'Place message here.'
+  })
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'Hello World!' })
+  }
+  return <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button
+      className='btn'
+      onClick={changeMessage}
+    >
+      Change Message
+      </button>  
+  </>
 };
 
 export default UseStateObject;
