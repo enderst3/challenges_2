71ce9487e5869a486aec71c7535fdd2e3f28e55c
enderst3 <enderst3@gmail.com>
2022-04-18 19:31:05 -0700

    project_2

--- react/react-project-2/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react/react-project-2/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -11,11 +11,20 @@ const UseStateCounter = () => {
     <section
       style = {{ margin: '4rem 0'}}
     >
-      <h2>Regular Counter</h2>
+      <h2>More Complex Counter</h2>
       <h1>{value}</h1>
-      <button className='btn' onClick={() => setValue (value-1)}>Decrease</button>
+      <button className='btn' >Decrease</button>
       <button className='btn' onClick={reset}>Reset</button>
       <button className='btn' onClick={() => setValue (value + 1)}>Increase</button>
+    </section>
+    <section
+      style = {{ margin: '4rem 0'}}
+    >
+      <h2>More Complex Counter</h2>
+      <h1>{value}</h1>
+      <button className='btn'>
+        Increase Later
+      </button>
     </section>
   </>
 };
