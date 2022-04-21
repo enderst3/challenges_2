a4bc0248a922bd7254b4f6d32633a198ab22423f
enderst3 <enderst3@gmail.com>
2022-04-20 20:42:14 -0700

    project_2

---
+++ react/react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <h3>0 Birthdays Today</h3>
+        <List />
+        <button onClick={() => console.log('You Clicked Me!')}>
+          Clear all
+        </button>
+      </section>
+    </main>
+  )
+}
+
+export default App;
