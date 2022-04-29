28cbb98a9bc9c092fc84564eafcc12fe8f7e4453
enderst3 <enderst3@gmail.com>
2022-04-28 20:03:12 -0700

    project_2

--- react/react-projects-master/02-tours/setup/src/App.js
+++ react/react-projects-master/02-tours/setup/src/App.js
@@ -4,8 +4,52 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
-}
 
+const App= () => {
+  const [tours, setTours] = useState([])
+
+  const getTours = async() => {
+    const response = await fetch(url)
+    const tours = await response.json()
+    setTours(tours)
+    console.log(tours)
+  }
+
+  const removeTour = (id) => {
+    let newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  useEffect( () => {
+    getTours()
+  }, [])
+
+  return (
+  <>
+    <h3>Tours</h3>
+    <ul className='tours'>
+      {tours.map((tour)=>{
+        const {id, name, info, image, price} = tour
+        return (
+          <li key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{info}</p>
+              <h4>{price}</h4>
+              <button
+                className='btn'
+                onClick = {() => removeTour(id)}
+              >
+                Not Interested
+              </button>
+            </div>
+
+          </li>
+        )
+      })}
+    </ul>
+  </>
+  )
+}
 export default App
