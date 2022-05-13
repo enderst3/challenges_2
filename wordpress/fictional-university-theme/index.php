d7d302c5a2fd94a50df0187c63fc0057526a8e60
enderst3 <enderst3@gmail.com>
2022-05-12 21:25:57 -0700

    wordpress

---
+++ wordpress/fictional-university-theme/index.php
@@ -0,0 +1,11 @@
+<?php
+    function greet($name, $color) {
+        echo "<p> Hi, my name is $name and my favorite color is $color.</p>";
+    
+    }
+    greet('John', 'green');
+    greet('Jane', 'pink');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<h4><?php bloginfo('description'); ?></h4>
