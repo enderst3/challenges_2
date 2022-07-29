df585ff23cf9736d3868a656813816da34478cbc
enderst3 <enderst3@gmail.com>
2022-07-28 19:18:30 -0700

    day6

--- python/100_Days/day_5.py
+++ python/100_Days/day_5.py
@@ -23,11 +23,94 @@ Example Output
 """
 
 student_heights = [180, 124, 165, 173, 189, 169, 146]
-count = 0
+students = 0
 total_height = 0
-for height in student_heights:
-    count += 1
-    total_height += height
-print(round(total_height/count))
+for student in student_heights:
+    students += 1
+    total_height += student
+print(round(total_height/students))
 
 print("=" * 15)
+
+'''
+You are going to write a program that calculates the sum of all the even numbers from 1 to 100. 
+Thus, the first even number would be 2 and the last one is 100:
+i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+Important, there should only be 1 print statement in your console output. 
+It should just print the final total and not every step of the calculation.
+Hint
+There are quite a few ways of solving this problem, but you will need to use the range() 
+function in any of the solutions.
+
+'''
+
+# I did this a few ways
+evens = []
+total = 0
+for i in range(1, 101):
+    if i % 2 == 0:
+        evens.append(i)
+        total += i
+# print(sum(evens))
+print(total)
+
+# Could also be soved using start, stop, step
+total = 0
+for i in range(2, 101, 2):
+    total += 1
+print(total)
+
+
+print("=" * 15)
+
+
+"""
+You are going to write a program that automatically prints the solution to the FizzBuzz game.
+Your program should print each number from 1 to 100 in turn.
+When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+  And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+"""
+
+for i in range(1, 100+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
+print("=" * 15)
+
+"""
+You are going to write a program that calculates the highest score from a List of scores.
+e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+Important you are not allowed to use the max or min functions. 
+The output words must match the example. i.e
+The highest score in the class is: x
+Example Input
+78 65 89 86 55 91 64 89
+In this case, student_scores would be a list that looks like: [78, 65, 89, 86, 55, 91, 64, 89]
+Example Output
+The highest score in the class is: 91
+"""
+
+🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+
+# not using max() for loop solution
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
---
+++ python/100_Days/day_6.py
@@ -0,0 +1,32 @@
+"""
+Day_6 
+Functions
+
+Most of the coding today was on reebogs world
+"""
+
+
+def my_function():
+    print("Hello World!")
+    print("Bye Bye World!")
+
+
+my_function()
+
+print("=" * 15)
+
+"""
+while loop
+
+Use while loops when you don't need to do something on each step or dont care what number,
+or item you are on, you just want to do something until the condition is false.  
+As long as its not an infinate loop
+
+while something_is_true:
+    do this
+    then do this
+"""
+counter = 6
+while counter > 0:
+    counter -= 1
+    print(counter)
