fad610849f64a556a1188fc676b8703f1d1be995
enderst3 <enderst3@gmail.com>
2022-08-03 18:50:28 -0700

    blackjack

--- python/100_Days/calulator/calc.py
+++ python/100_Days/calulator/calc.py
@@ -4,34 +4,79 @@ Create a calulator
 
 from art import logo
 
-
-def add(a, b):
-    return a + b
+# Add
 
 
-def subtract(a, b):
-    return a - b
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
 
 
-def multiply(a, b):
-    return a * b
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
 
 
-def divide(a, b):
-    return(a, b)
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
 
 
-a = int(input("Enter your first number: \n"))
-math_option = input("Chose what math you want to do: \n + \n - \n * \n / \n")
-b = int(input("Now enter your second numer: \n"))
+def divide(n1, n2):
+    return(n1, n2)
 
-if math_option == "+":
-    print(add(a, b))
-elif math_option == "-":
-    print(subtract(a, b))
-elif math_option == "*":
-    print(multiply(a, b))
-elif math_option == "/":
-    print(divide(a, b))
-else:
-    print("Error.  Invalid operator.")
+
+# n1 = int(input("Enter your first number: \n"))
+# math_option = input("Chose what math you want to do: \n + \n - \n * \n / \n")
+# n2 = int(input("Now enter your second numer: \n"))
+
+# if math_option == "+":
+#     print(add(n1, n2))
+# elif math_option == "-":
+#     print(subtract(n1, n2))
+# elif math_option == "*":
+#     print(multiply(n1, n2))
+# elif math_option == "/":
+#     print(divide(n1, n2))
+# else:
+#     print("Error.  Invalid operator.")
+
+"""
+Class version of calculator
+"""
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    print(logo)
+    num1 = float(input("Enter your first number: \n"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: \n"))
+        calculaiton_function = operations[operation_symbol]
+        answer = calculaiton_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
