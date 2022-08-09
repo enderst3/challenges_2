416032a053fe9ed0c4e2dab01316fd7ee972005e
enderst3 <enderst3@gmail.com>
2022-08-08 18:20:50 -0700

    hilow

--- python/100_Days/higher_lower/hi_low.py
+++ python/100_Days/higher_lower/hi_low.py
@@ -3,9 +3,7 @@ import os
 from art import logo, vs
 from game_data import data
 
-print(logo)
 
-# os.system('clear')
 # pick random account
 
 
@@ -25,11 +23,45 @@ def format_data(account):
 # check answers
 
 
-account_a = get_random_account()
-print(format_data(account_a))
-print(vs)
-account_b = get_random_account()
-print(format_data(account_b))
+def check_answers(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ")
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answers(guess, a_follower_count, b_follower_count)
+
+        os.system('clear')
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}")
+        else:
+            game_should_continue = False
+            print(f"Sorry, That's wrong.  Final score: {score}")
+
+
+game()
+
 # put everything in a function
 # display logo
 # get 2 random items from data
