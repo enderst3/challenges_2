a3b330778694deac90a7d3e668afe2ec19aa1395
enderst3 <enderst3@gmail.com>
2022-08-27 22:24:13 -0700

    turtle

--- python/day_18/main.py
+++ python/day_18/main.py
@@ -1,9 +1,14 @@
 import turtle as tim
 from turtle import Turtle, Screen
+import random
 from random import randint
 
 tim.colormode(255)
+directions = [0, 90, 180, 270]
+tim.pensize(16)
+tim.speed('fastest')
 
+'''
 def draw_shape(num_sides):
     angle = 360 / num_sides
     tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
@@ -14,6 +19,12 @@ def draw_shape(num_sides):
 
 for num_angles in range(3, 11):
     draw_shape(num_angles)
+'''
+
+for _ in range(200):
+    tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
+    tim.setheading(random.choice(directions))
+    tim.forward(30)
 
 screen = Screen()
 screen.exitonclick()
