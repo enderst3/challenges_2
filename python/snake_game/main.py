d6528a261e859a93c9f9928d296d32b141c4cdc6
enderst3 <enderst3@gmail.com>
2022-09-14 22:18:53 -0700

    snake notes

--- python/snake_game/main.py
+++ python/snake_game/main.py
@@ -24,13 +24,15 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    # for loop using range start, step, stop to move snake body
+    # for loop using range start, step, stop to move snake body.
+    # We start on the last segment -1, stop on the first segment, and we move back one step -1
     for seg_num in range(len(segments) - 1, 0, -1):
         new_x = segments[seg_num - 1].xcor()
         new_y = segments[seg_num - 1].ycor()
         segments[seg_num].goto(new_x, new_y)
+    # move the first segment forward, and other segments will follow
     segments[0].forward(20)
-    
+
 
 
 
