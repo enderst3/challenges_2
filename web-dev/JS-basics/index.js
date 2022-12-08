eeeadf420e8fd0666d49c873768304a2c3f486c8
enderst3 <enderst3@gmail.com>
2022-12-07 20:26:45 -0800

    js

--- web-dev/JS-basics/index.js
+++ web-dev/JS-basics/index.js
@@ -13,3 +13,51 @@ typeof(25);
 typeof("Hello");
 
 typeof(true);
+
+Variables
+
+var
+const
+let
+
+const name = prompt("What is your name?");
+
+alert(name);
+
+// Challenge 1
+
+function test() {
+    var a = "3";
+    var b = "8";
+
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+
+// Destructuring solution
+//[a,b] = [b,a];
+
+// Using an extra Variable
+var c = a;
+var a = b;
+var b = c;
+
+
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+
+//Strings
+
+alert ("Hello" + " World");
+
+const message = "Hello";
+let name = prompt("What is your name? ");
+
+alert(message + " there " + name)
+
+const word = "Hello"
+
+console.log(word.length);
