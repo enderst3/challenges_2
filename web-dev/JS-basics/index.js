0bfbe997e8d8bf79e24c995e5ab30a681ba0ea23
enderst3 <enderst3@gmail.com>
2022-12-16 19:55:48 -0800

    bmi

--- web-dev/JS-basics/index.js
+++ web-dev/JS-basics/index.js
@@ -208,8 +208,16 @@ function lifeInWeeks(age) {
 function bmiCalculator(weight, height) {
   let bmi = Math.round(weight / (height * height));
   // could also use Math.pow(height(2))
-  return bmi;
-}
+  if (bmi < 18.5) {
+    return "Your BMI is " + bmi + ", so you are underweight."
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Your BMI is " + bmi + ", so you have a normal weight."
+  }
+  if (bmi > 24.9) {
+    return "Your BMI is " + bmi + ", so you are overweight."
+  }
+
 
 
 // love generator
