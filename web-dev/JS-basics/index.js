085d2557be75d2368034bfae4a689e910cbbfc44
enderst3 <enderst3@gmail.com>
2022-12-18 11:33:56 -0800

    fizzbuzz

--- web-dev/JS-basics/index.js
+++ web-dev/JS-basics/index.js
@@ -287,3 +287,42 @@ let guest = prompt("Enter your name. ")
 if (guestList.includes(guest)) {
   console.log("yes")
 }
+
+
+// fizz buzz
+
+for (var i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz")
+  }
+  if (i % 3 === 0) {
+    console.log("Fizz")
+  }
+  if (i % 5 === 0) {
+    console.log("Buzz")
+  } else {
+    console.log(i)
+  }
+}
+
+
+// fizz buzz list output Functions
+let output = [];
+let count = 1;
+
+function fizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  }
+
+  else if (count % 3 === 0) {
+    output.push("Fizz");
+  }
+  else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count)
+  }
+  count ++;
+  console.log(output);
+}
