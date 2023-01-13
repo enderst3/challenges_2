9a3f2f3f5a6b0a4734857aa0d09959ddd1e9045a
enderst3 <enderst3@gmail.com>
2023-01-13 14:36:27 -0800

    event listner

--- web-dev/jQuery/index.js
+++ web-dev/jQuery/index.js
@@ -1,4 +1,15 @@
 
-$("h1").click(function() {
+
+$("button").click(function() {
   $("h1").css("color", "purple")
 })
+
+$("input").keydown(function(event) {
+  $("h1").text(event.key)
+})
+
+
+// best way
+$("h1").on("mouseover", (function() {
+  $("h1").css("color", "black")
+}))
