14d153c71f1eb7ffd69ed4ed359aa1d3849ec3b7
enderst3 <enderst3@gmail.com>
2023-01-27 02:33:15 -0800

    calc start'

--- web-dev/my-express-server/server.js
+++ web-dev/my-express-server/server.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 
+// start server
 app.listen(3000, function() {
   console.log("Server started on port 3000")
 })
 
+app.get("/about", function(req, res) {
+  res.send("Brief bio gose here.  It will be completed someday.")
+})
+
+// Bio route route
+app.get("/contact", function(req, res) {
+  res.send("Contact me at: me@gmail.com")
+})
+
+// Home route
 app.get("/", function(req, res) {
   res.send("<h1>Hello World!</h1>")
 })
