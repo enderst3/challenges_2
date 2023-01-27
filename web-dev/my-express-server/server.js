21c00b5456f316e756de064beef0190bbe241017
enderst3 <enderst3@gmail.com>
2023-01-26 18:58:49 -0800

    node server

--- web-dev/my-express-server/server.js
+++ web-dev/my-express-server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
-// app.listen(3000, function() {
-//   console.log("Server started on port 3000")
-// })
+app.listen(3000, function() {
+  console.log("Server started on port 3000")
+})
 
-app.get('/', (req, res) => res.send("Hello World!"))
+app.get("/", function(req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"))
+// app.get('/', (req, res) => res.send("Hello World!"))
+//
+// app.listen(3000, () => console.log("Example app listening on port 3000!"))
