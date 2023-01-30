e12245f9a8f465ab84ecb6cde0034c04705ec1b2
enderst3 <enderst3@gmail.com>
2023-01-29 20:00:16 -0800

    weather

--- web-dev/weather-project/app.js
+++ web-dev/weather-project/app.js
@@ -4,10 +4,21 @@ const https = require("https")
 const app = express()
 
 app.get("/", (req, res) => {
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=41.62&lon=-109.46&appid=96248b485fca8f84065e794b16e74f1d"
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=41.62&lon=-109.46&appid=96248b485fca8f84065e794b16e74f1d&units=imperial"
   https.get(url, (response) => {
-    console.log(response)
+    console.log(response.statusCode)
+
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const conditions = weatherData.weather[0].description
+      const location = weatherData.name + ', ' + weatherData.sys.country
+      console.log(temp)
+      console.log(conditions)
+      console.log(location)
+    })
   })
+
   res.send("Server is up and running!")
 })
 
