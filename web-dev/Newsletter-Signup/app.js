39b7f878a75b0e4ee14dd2a15e294d148dee0a18
enderst3 <enderst3@gmail.com>
2023-02-01 20:23:56 -0800

    finish newsletter

--- web-dev/Newsletter-Signup/app.js
+++ web-dev/Newsletter-Signup/app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
+const https = require("https")
 
-const app = express()
+let app = express()
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -15,11 +16,52 @@ app.post("/", (req, res) => {
   let firstName = req.body.fName
   let lastName = req.body.lName
   let email = req.body.email
+  const apiKey = { your api key }
+  const listID = { Your list id }
 
-  console.log(firstName, lastName, email)
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+  let jsonData = JSON.stringify(data)
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/" + listID
+
+  const options = {
+    method: "POST",
+    auth: "me1:" + apiKey
+  }
+
+  let request = https.request(url, options, (response) => {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+    // response.on("data", (data) => {
+    //   console.log(JSON.parse(data))
+    // })
+  })
+
+  request.write(jsonData)
+  request.end()
+
 })
 
+app.post("/failure", (req, res) => {
+  res.redirect("/")
+})
 
-app.listen(3000, () => {
+app.listen(process.envPORT || 3000, () => {
   console.log("server started on port 3000!")
 })
