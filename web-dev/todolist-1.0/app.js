00e0c13fad33082254555e523b6fa5cce9943cac
enderst3 <enderst3@gmail.com>
2023-02-03 18:56:40 -0800

    added ejs

--- web-dev/todolist-1.0/app.js
+++ web-dev/todolist-1.0/app.js
@@ -1,11 +1,45 @@
-
 const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  let today = new Date()
+  let currentDay = today.getDay()
+  let day = ""
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday"
+      break;
+    case 1:
+      day = "Monday"
+      break;
+    case 2:
+      day = "Tuesday"
+      break;
+    case 3:
+      day = "Wednesday"
+      break;
+    case 4:
+      day = "Thursday"
+      break;
+    case 5:
+      day = "Friday"
+      break;
+    case 6:
+      day = "Saturday"
+      break;
+    default:
+    console.log("Error: Current day is equal to: " + currentDay)
+
+  }
+
+  res.render("list", {
+    kindOfDay: day
+  })
 })
 
 app.listen(3000, (req, res) => {
