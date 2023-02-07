dcf7bc827f007b96fe9974f3754c5df9b13c9066
enderst3 <enderst3@gmail.com>
2023-02-06 22:15:17 -0800

    todo list

--- web-dev/todolist-1.0/app.js
+++ web-dev/todolist-1.0/app.js
@@ -3,7 +3,11 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+let item = ""
+
 app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
   let today = new Date()
@@ -18,8 +22,16 @@ app.get("/", (req, res) => {
 
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItem: item
   })
+})
+
+app.post("/", (req, res) => {
+  item = req.body.newItem
+  // console.log(item)
+  // trigger app.get and pass in item
+  res.redirect("/")
 })
 
 app.listen(3000, (req, res) => {
