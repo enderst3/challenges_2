469632ea8e095ee9e67476ec6cab99ed27866c54
enderst3 <enderst3@gmail.com>
2023-02-08 19:53:01 -0800

    css

--- web-dev/todolist-1.0/app.js
+++ web-dev/todolist-1.0/app.js
@@ -8,6 +8,7 @@ let items= ["Buy Food"]
 app.set("view engine", "ejs")
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
   let today = new Date()
