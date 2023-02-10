fa055394461d54fb02e81d3d2eebaa427b9905c6
enderst3 <enderst3@gmail.com>
2023-02-09 21:09:25 -0800

    work route

--- web-dev/todolist-1.0/app.js
+++ web-dev/todolist-1.0/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser")
 const app = express()
 
 let items= ["Buy Food"]
+let workItems = []
 
 app.set("view engine", "ejs")
 
@@ -23,16 +24,36 @@ app.get("/", (req, res) => {
 
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   })
 })
 
 app.post("/", (req, res) => {
+
   let item = req.body.newItem
-  items.push(item)
-  // trigger app.get and pass in item
-  res.redirect("/")
+
+  if (req.body.list === "Work"){
+    workItems.push(item)
+    res.redirect("/work")
+  } else {
+    items.push(item)
+    // trigger app.get and pass in item
+    res.redirect("/")
+  }
+})
+
+app.get("/work", (req,res) => {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  })
+})
+
+app.post("/work", (req,res) => {
+  let item = req.body.newListItem
+  workItems.push(item)
+  res.redirect("/work")
 })
 
 app.listen(3000, (req, res) => {
