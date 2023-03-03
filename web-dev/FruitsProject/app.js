b88cb4f61329faf2f9a4c735b63c906290605a77
enderst3 <enderst3@gmail.com>
2023-03-02 22:52:22 -0800

    fruit

--- web-dev/FruitsProject/app.js
+++ web-dev/FruitsProject/app.js
@@ -8,7 +8,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'fruitsDB';
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
@@ -17,5 +17,49 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  client.close();
+  findDocuments(db, function() {
+    client.close();
+  });
 });
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {
+      name: "Apple",
+      score: 6,
+      review: "Great fruit.  Will eat more"
+    },
+    {
+      name: "Orange",
+      score: 8,
+      review: "Orange you happy I didn't say Banana!"
+    },
+    {
+      name: "Banana",
+      score: 9,
+      review: "I would have scored it a 10 but I slipped on the peel."
+    }
+
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits)
+    callback(fruits);
+  });
+}
