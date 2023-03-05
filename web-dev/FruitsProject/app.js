5f3f64992126b68d8e43ae4f63ffb6f00a60c62f
enderst3 <enderst3@gmail.com>
2023-03-04 22:13:15 -0800

    save entry to schema

--- web-dev/FruitsProject/app.js
+++ web-dev/FruitsProject/app.js
@@ -2,21 +2,26 @@ const mongoose = require('mongoose')
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB")
 
+// build schema
 const fruitSchema = new mongoose.Schema ({
   name: String,
   rating: Number,
   review: String
 })
 
+
+// Create model
 const Fruit = mongoose.model("Fruit", fruitSchema)
 
+// create entry
 const fruit = new Fruit ({
   name: "Apple",
   rating: 8,
   review: "Great fruit.  Will eat more"
 })
 
-
+// save entry
+fruit.save()
 
 const findDocuments = function(db, callback) {
   // Get the documents collection
