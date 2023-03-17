d919060af1dae1e962743c3b0727e892ae799b86
enderst3 <enderst3@gmail.com>
2023-03-16 19:56:18 -0700

    new fruit, people

--- web-dev/FruitsProject/app.js
+++ web-dev/FruitsProject/app.js
@@ -1,24 +1,41 @@
 
 const mongoose = require('mongoose')
 
-// connect to db
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true })
+// connect to server, look for db if not there create db
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true } )
 
-// create schema
+
+// Create Schema for new collection
 const fruitSchema = new mongoose.Schema ({
   name: String,
   rating: Number,
   review: String
 })
 
-// Create Mongoose model will create fruits
+// Create mongoos model
+// specify singular name.  Will creat a collection of plural items
 const Fruit = mongoose.model("Fruit", fruitSchema)
 
-// create document in model
-const fruit = new Fruit ({
+// add entry to model
+const fruit = new Fruit({
   name: "Apple",
   rating: 7,
-  review: "Would be better with caramel but still better than an orange."
+  review: "Pretty good, would be better baked or with caramel, but still way better than an orange!"
 })
 
-fruit.save()
+// save new fruit
+// fruit.save()
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+})
+
+const People = mongoose.model("Person", personSchema)
+
+const person = new Person ({
+  name: "Johnny Appleseed",
+  age: 32
+})
+
+person.save()
