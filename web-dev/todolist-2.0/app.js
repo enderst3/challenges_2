70de1b66afc187fd40a00056bdb08573ab386f88
enderst3 <enderst3@gmail.com>
2023-03-22 22:03:30 -0700

    todo db

--- web-dev/todolist-2.0/app.js
+++ web-dev/todolist-2.0/app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://0.0.0.0:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
 const itemsSchema = {
   name: String
@@ -39,18 +39,25 @@ const item3 = new Item ({
 
 const defaultItems = [item1, item2, item3]
 
-Item.insertMany(defaultItems, function(err){
-  if (err) {
-    console.log(err)
-  } else {
-    console.log("Items successfully saved to DB.")
-  }
-})
+
 
 app.get("/", function(req, res) {
 
-  res.render("list", {listTitle: "Today", newListItems: items});
-
+  // find all items in collect
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Items successfully saved to DB.")
+        }
+      })
+      res.redirect("/")
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  })
 });
 
 app.post("/", function(req, res){
