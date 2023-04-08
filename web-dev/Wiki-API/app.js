8d98a972c68c826884a6b6edaa61767f07769434
enderst3 <enderst3@gmail.com>
2023-04-07 21:56:27 -0700

    wikiapi

---
+++ web-dev/Wiki-API/app.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(express.static("public"))
+
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB",{
+  useNewUrlParser: true
+})
+
+const articleSchema = {
+  title: String,
+  content: String
+}
+
+const Article = mongoose.model(
+  "Article",
+  articleSchema
+)
+
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticles){
+    if (!err) {
+      res.send(foundArticles)
+    } else {
+      res.send(err)
+    }
+  })
+})
+
+app.post("/articles", function(req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  })
+  newArticle.save(function(err) {
+    if (!err){
+      res.send("Successfully added new article.")
+    } else {
+      res.send(err)
+    }
+  })
+})
+
+app.delete("/articles", function(req, res) {
+
+})
+
+
+app.listen(3000, function() {
+  console.log("server started on port 3000")
+})
