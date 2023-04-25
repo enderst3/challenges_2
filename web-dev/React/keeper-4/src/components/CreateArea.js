78f8701551046422744c5e352c094770184dd28c
enderst3 <enderst3@gmail.com>
2023-04-24 18:15:42 -0700

    keeper final

--- web-dev/React/keeper-4/src/components/CreateArea.js
+++ web-dev/React/keeper-4/src/components/CreateArea.js
@@ -5,6 +5,9 @@ import Zoom from '@mui/material/Zoom';
 
 
 function CreateArea(props) {
+
+  const [isExpanded, setIsExpanded]  = useState(false)
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -30,23 +33,29 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setIsExpanded(true)
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
+        {isExpanded ? <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
+        /> : null}
+
         <textarea
           name="content"
+          onClick={expand}
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <Zoom in={true}>
+        <Zoom in={isExpanded}>
             <Fab onClick={submitNote}>
                 <AddIcon />
             </Fab>
